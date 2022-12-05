3f9e9fb541108192013e76ec2750459af0910d05
achchaimae <achchaimae1@gmail.com>
2022-12-05 16:10:38 +0100

    note and style 🐾

--- view/Note.php
+++ view/Note.php
@@ -1,22 +1,28 @@
-
-
-<?php 
-
-echo $title;
-echo "<br>";
-echo $name;
-echo "<br>";
-echo $email;
-?>
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ToDo</title>
+    <link rel="stylesheet" href='css/style.css' type="text/css">
+</head>
 <body>
-
-<form action="/notes" method="post">
-    Name: <input type="text" name="name" ><br>
-    E-mail: <input type="text" name="email"><br>
-    <input type="submit">
-</form>
-
+    <div class="todo-content">
+        <div class="add-task">
+            <input type="text" placeholder="new task">
+            <button>add</button>
+        </div>
+        <div class="tasks">
+        <input type="checkbox" name="task" id="">
+            <label for="checkbox">task</label><br>
+            <input type="checkbox" name="task" id="">
+            <label for="checkbox">task</label><br>
+            <input type="checkbox" name="task" id="">
+            <label for="checkbox">task</label><br>
+            <input type="checkbox" name="task" id="">
+            <label for="checkbox">task</label>
+        </div>
+    </div>
 </body>
 </html>
